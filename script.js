7ef57388cb651e8e96af0dc439e420fb7f852dd4
Henrique da Silva <henriquesilvarosa80@gmail.com>
2024-06-11 19:09:07 -0300

    new

--- script.js
+++ script.js
@@ -1,79 +1,92 @@
+document.getElementById('formDoacao').addEventListener('submit', function(event) {
+    event.preventDefault()
 
-// document.getElementById('formDoacao').addEventListener('submit', function(event) {
-//     event.preventDefault(); 
+    var nomeDoador = document.getElementById('nomeDoador').value
+    var tipoAlimento = document.getElementById('tipoAlimento').value
+    var quantidade = document.getElementById('quantidade').value
+    var dataValidade = document.getElementById('dataValidade').value
+    var numeroDoador = document.getElementById('numeroDoador').value
 
-//     var nomeDoador = document.getElementById('nomeDoador').value;
-//     var tipoAlimento = document.getElementById('tipoAlimento').value;
-//     var quantidade = document.getElementById('quantidade').value;
-//     var dataValidade = document.getElementById('dataValidade').value;
-//     var numeroDoador = document.getElementById('numeroDoador').value; 
+    var doacao = {
+        "nomeDoador": nomeDoador,
+        "tipoAlimento": tipoAlimento,
+        "quantidade": quantidade,
+        "dataValidade": dataValidade,
+        "numeroDoador": numeroDoador
+    };
 
-//     var doacao = {
-//         "nomeDoador": nomeDoador,
-//         "tipoAlimento": tipoAlimento,
-//         "quantidade": quantidade,
-//         "dataValidade": dataValidade,
-//         "numeroDoador": numeroDoador
-//     };
-
-//     fetch('http://localhost:3000/doacoes', { 
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(doacao)
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log('Doação enviada com sucesso:', data);
-//         document.getElementById('paginaInicial').style.display = 'none'; 
-//         document.getElementById('paginaResultados').style.display = 'block'; 
-//         var organizacoesLista = document.getElementById('organizacoesLista');
-//         var li = document.createElement('li');
-//         li.innerHTML = '<p><strong>Nome do Doador:</strong> ' + nomeDoador + '</p><p>Tipo de Alimento: ' + tipoAlimento + '</p><p>Quantidade: ' + quantidade + '</p><p>Data de Validade: ' + dataValidade + '</p>';
-//         organizacoesLista.appendChild(li);
-
-//         var btnContato = document.createElement('button');
-//         btnContato.textContent = 'Entrar em Contato';
-//         btnContato.className = 'btnVerde';
-//         organizacoesLista.appendChild(btnContato);
-
-//         btnContato.addEventListener('click', function() {
-//             var mensagemDoacao = document.createElement('div');
-//             mensagemDoacao.textContent = 'Número do Doador: ' + numeroDoador;
-//             organizacoesLista.appendChild(mensagemDoacao);
-//         });
-//     })
-//     .catch(error => {
-//         console.error('Erro ao enviar doação:', error);
-//     });
-// });
-
-
-function addDoacoes() {
-    const nomeDoador = document.getElementById('nomeDoador').value;
-    const numeroDoador = document.getElementById('numeroDoador').value;
-    const tipoAlimento = document.getElementById('tipoAlimento').value;
-    const quantidade = document.getElementById('quantidade').value;
-    const dataValidade = document.getElementById('dataValidade').value;
-    
     fetch('http://localhost:3000/doacoes', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({
-            nomeDoador,
-            numeroDoador,
-            tipoAlimento,
-            quantidade,
-            dataValidade
-        }),
+        body: JSON.stringify(doacao)
     })
     .then(response => response.json())
     .then(data => {
-      alert(data.mensagem);
+        console.log('Doação enviada com sucesso:', data)
+        document.getElementById('paginaInicial').style.display = 'none'
+        document.getElementById('paginaResultados').style.display = 'block'
 
+        carregarDoacoes()
     })
-    .catch(error => console.error("Erro:", error));
+    .catch(error => {
+        console.error('Erro ao enviar doação:', error);
+    });
+});
+
+function deletarDoacoes(id) {
+    fetch(`http://localhost:3000/doacoes/${id}`, {
+        method: 'DELETE'
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Doação excluída com sucesso:', data)
+        carregarDoacoes()
+    })
+    .catch(error => {
+        console.error('Erro ao excluir doação:', error)
+    });
 }
+
+function carregarDoacoes() {
+    fetch('http://localhost:3000/doacoes')
+        .then(response => response.json())
+        .then(doacoes => {
+            const organizacoesLista = document.getElementById('organizacoesLista');
+            organizacoesLista.innerHTML = '' // Limpa o conteúdo atual da lista
+            
+            // Itera sobre cada doação e cria um elemento para exibi-la na lista
+            doacoes.forEach(doacao => {
+                const li = document.createElement('li')
+                li.classList.add('doacao')
+                li.innerHTML = `
+                    <h2>Nome do Doador: ${doacao.nomeDoador}</h2>
+                    <p><strong>Tipo de Alimento:</strong> ${doacao.tipoAlimento}</p>
+                    <p><strong>Quantidade:</strong> ${doacao.quantidade}</p>
+                    <p><strong>Data de Validade:</strong> ${doacao.dataValidade}</p>
+                    <button class="btnVerde">Entrar em Contato</button>
+                    <button class="btnExcluir">Excluir</button>
+                    <div class="mensagemDoacao" style="display: none;">Número do Doador: ${doacao.numeroDoador}</div>
+                `;
+                organizacoesLista.appendChild(li)
+
+                const btnContato = li.querySelector('.btnVerde');
+                const mensagemDoacao = li.querySelector('.mensagemDoacao');
+                btnContato.addEventListener('click', function() {
+                    mensagemDoacao.style.display = 'block'
+                });
+
+                const btnExcluir = li.querySelector('.btnExcluir');
+                btnExcluir.addEventListener('click', function() {
+                    deletarDoacoes(doacao.id)
+                });
+            });
+        })
+        .catch(error => console.error('Erro ao carregar as doações:', error))
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('paginaResultados').style.display = 'block'
+    carregarDoacoes()
+})
--- server.js
+++ server.js
@@ -1,47 +1,53 @@
+const express = require('express')
+const fs = require('fs')
+const cors = require('cors')
 
-const express = require('express');
-const fs = require('fs');
-const cors = require('cors');
-
-const app = express();
-const PORT = 3000;
-app.use(cors());
+const app = express()
+const PORT = 3000
+app.use(cors())
 app.use(express.json())
 
 const dados = require("./data/organizacoes.json")
 
-
 app.get('/doacoes', (req, res) => {
-
-        return res.json(dados.doacoes);
-    
+    return res.json(dados.doacoes)
 });
-
 
 app.post('/doacoes', (req, res) => {
-    const novoDoacao = req.body;
+    const novoDoacao = req.body
 
     try {
-        const ultimoId = dados.doacoes.length > 0 ? dados.doacoes[dados.doacoes.length - 1].id : 0;
-        novoDoacao.id = ultimoId + 1;
+        const ultimoId = dados.doacoes.length > 0 ? dados.doacoes[dados.doacoes.length - 1].id : 0
+        novoDoacao.id = ultimoId + 1
 
-        dados.doacoes.push(novoDoacao);
-        salvarDados(dados);
+        dados.doacoes.push(novoDoacao)
+        salvarDados(dados)
 
-        return res.status(201).json({ mensagem: "Sucesso" });
+        return res.status(201).json({ mensagem: "Sucesso" })
     } catch (error) {
-        console.error('Erro ao salvar a doação:', error);
-        return res.status(500).json({ mensagem: "Erro ao salvar a doação" });
+        console.error('Erro ao salvar a doação:', error)
+        return res.status(500).json({ mensagem: "Erro ao salvar a doação" })
     }
 });
 
+app.delete('/doacoes/:id', (req, res) => {
+    const id = parseInt(req.params.id)
 
-
-function salvarDados(){
-    fs.writeFileSync(__dirname + "/data/organizacoes.json",JSON.stringify(dados,null,2))
+    const index = dados.doacoes.findIndex(doacao => doacao.id === id)
+    if (index !== -1) {
+        dados.doacoes.splice(index, 1)
+        salvarDados(dados)
+        return res.status(200).json({ mensagem: "Doação deletada com sucesso" })
+    } else {
+        return res.status(404).json({ mensagem: "Doação não encontrada" })
     }
+})
 
+function salvarDados() {
+    fs.writeFileSync(__dirname + "/data/organizacoes.json", JSON.stringify(dados, null, 2))
+}
 
+// Inicia o servidor
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log(`Servidor rodando em http://localhost:${PORT}`)
 });
